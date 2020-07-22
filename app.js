b74772a8d624903cc0be5941e04c6950f194b45a
LoneStar <ernieurzua@gmail.com>
2020-07-22 00:11:44 -0700

    created app.js and package

---
+++ app.js
@@ -0,0 +1,14 @@
+//add departments, toles and employees
+
+
+//view departments, roles,employess
+
+//update employee roles
+
+/*bonus
+-update employee managers
+-view employees by manager
+-delete departments,roles, and employees
+-view the total utilized budget of a department
+i.e. the combined saleries of all employees in the department
+*/
