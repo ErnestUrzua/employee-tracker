ea2b163c0110aa13c91a2cb84071cda18b5e9a1f
LoneStar <ernieurzua@gmail.com>
2020-07-22 00:26:35 -0700

    added requires for mysql and inquirer, created connection to sql db

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+var mysql = require("mysql");
+var inquirer = require("requirer");
+
+// add connection to mysql server
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3300,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "employeesDB"
+});
+
+
+//connect to server
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    createProduct();
+
+});
 //add departments, toles and employees
 
 
