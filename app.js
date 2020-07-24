23d550b1adf73171614b4a21788eb190ad484e10
LoneStar <ernieurzua@gmail.com>
2020-07-23 21:41:59 -0700

    added inquirer prompts and package dependencies

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var mysql = require("mysql");
-var inquirer = require("requirer");
+var inquirer = require("inquirer");
 
 // add connection to mysql server
 var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port; if not 3306
-    port: 3300,
+    port: 3306,
 
     // Your username
     user: "root",
@@ -21,11 +21,38 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
 
 });
-//add departments, toles and employees
 
+//prompt user for information
+function start() {
+var inquirer = inquirer.prompt({
+    name: "choices",
+    type: "rawlist",
+    message: "What would you like to do??",
+    choices: ["add employee", "add department", "add role"]
+}).then(answer => {
+
+});
+}
+
+start();
+
+//add departments, roles and employees
+//query the db to add an employee
+function addEmployee() {
+    console.log("adding Employee\n");
+    connection.query("INSERT INTO employee (first_name) VALUES (?)", [req.body.first_name], function (err, results) {
+        if (err) throw err;
+    });
+
+    connection.query("INSERT INTO employee (last_name) VALUES (?)", [req.body.last_name], function (err, results) {
+        if (err) throw err;
+    });
+
+    // logs the actual query being run
+    console.log(query.sql);
+}
 
 //view departments, roles,employess
 
