aaebce88ece100d7a2ce57686fd46fa9cc89caac
LoneStar <ernieurzua@gmail.com>
2020-07-24 23:23:54 -0700

    added complete addEmployee function with prompts and inserting into the db

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "password",
-    database: "employeesDB"
+    database: "employees_db"
 });
 
 
@@ -26,14 +26,48 @@ connection.connect(function (err) {
 
 //prompt user for information
 function start() {
-var inquirer = inquirer.prompt({
-    name: "choices",
-    type: "rawlist",
-    message: "What would you like to do??",
-    choices: ["add employee", "add department", "add role"]
-}).then(answer => {
+    inquirer.prompt({
+        name: "choices",
+        type: "list",
+        message: "What would you like to do??",
+        choices: ["add employee", "add department", "add role", "exit"]
+    }).then(function (answer) {
+        switch (answer.choices) {
+            //CREATE
+            case "add employee":
+                addEmployee();
+                break;
+            case "add department":
+                addDepartment();
+                break;
+            case "add role":
+                addRole();
+                break;
+            //Read
+            case "view employee":
+                viewEmployee();
+                break;
+            case "view department":
+                viewDepartment();
+                break;
+            case "view role":
+                viewRole();
+                break;
+            //Update
+            case "update employee":
+                updateEmployee();
+                break;
+            case "update role":
+                viewRole();
+                break;
+            //DELETE BONUS
 
-});
+            case "exit":
+                //exit the prompt
+                break;
+
+        }
+    })
 }
 
 start();
@@ -41,6 +75,49 @@ start();
 //add departments, roles and employees
 //query the db to add an employee
 function addEmployee() {
+
+    inquirer.prompt([{
+        name: "firstName",
+        type: "list",
+        message: "first name:"
+    },
+    {
+        name: "lastName",
+        type: "list",
+        message: " last name:"
+    },
+    {
+        name: "roleID",
+        type: "list",
+        message: "role id:"
+    },
+    {
+        name: "managersID",
+        type: "list",
+        message: "managers id:"
+    },
+
+    ]);
+    console.log("adding Employee\n");
+    connection.query("INSERT INTO employee (first_name) VALUES (?)", [req.body.first_name], function (err, results) {
+        if (err) throw err;
+    });
+    connection.query("INSERT INTO employee (last_name) VALUES (?)", [req.body.last_name], function (err, results) {
+        if (err) throw err;
+    });
+    connection.query("INSERT INTO employee (roleID) VALUES (?)", [req.body.role_id], function (err, results) {
+        if (err) throw err;
+    });
+    connection.query("INSERT INTO employee (managersID) VALUES (?)", [req.body.manager_id], function (err, results) {
+        if (err) throw err;
+    });
+
+    // logs the actual query being run
+    console.log(query.sql);
+    start();
+}
+
+function addRole() {
     console.log("adding Employee\n");
     connection.query("INSERT INTO employee (first_name) VALUES (?)", [req.body.first_name], function (err, results) {
         if (err) throw err;
@@ -52,7 +129,24 @@ function addEmployee() {
 
     // logs the actual query being run
     console.log(query.sql);
+    start();
 }
+
+function addDepartment() {
+    console.log("adding Employee\n");
+    connection.query("INSERT INTO employee (first_name) VALUES (?)", [req.body.first_name], function (err, results) {
+        if (err) throw err;
+    });
+
+    connection.query("INSERT INTO employee (last_name) VALUES (?)", [req.body.last_name], function (err, results) {
+        if (err) throw err;
+    });
+
+    // logs the actual query being run
+    console.log(query.sql);
+    start();
+}
+
 
 //view departments, roles,employess
 
